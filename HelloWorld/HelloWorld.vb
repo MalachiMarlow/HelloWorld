eecdb3bd6f777f9bd25b0f3e34f1392fac31d73c
Tusk <Tuskman045@gmail.com>
2025-01-18 23:56:24 -0700

    Added Operator Info.

--- HelloWorld/HelloWorld.vb
+++ HelloWorld/HelloWorld.vb
@@ -1,4 +1,9 @@
-﻿Module HelloWorld
+﻿'Malachi Marlow
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/MalachiMarlow/HelloWorld.git
+Module HelloWorld
 
     Sub Main()
         Console.WriteLine("Hello, World!")
